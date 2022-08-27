b47a053d0be66204f287265ca6d96226daf44081
Arian Hosseini <arianhosseini@outlook.com>
2022-08-28 02:15:45 +0430

    refactor: configured all routes

--- src/components/FAB/FAB.jsx
+++ src/components/FAB/FAB.jsx
@@ -1,22 +1,31 @@
 import { useState } from "react";
-import { useTranslation } from "react-i18next";
 
 import SpeedDial from "@mui/material/SpeedDial";
 import SpeedDialAction from "@mui/material/SpeedDialAction";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLanguage } from "@fortawesome/free-solid-svg-icons";
 
+import { useTranslation } from "react-i18next";
+
 import EnglishIcon from "../../assets/images/english-icon.png";
 import PersianIcon from "../../assets/images/persian-flag.png";
 
 import "./FAB.css";
 
-export default function FAB({ changeLanguage }) {
+export default function FAB() {
+	// eslint-disable-next-line
 	const { t, i18n } = useTranslation();
 
 	const [open, setOpen] = useState(false);
 	const handleOpen = () => setOpen(true);
 	const handleClose = () => setOpen(false);
+
+	// Change Language Event Handler
+	const changeLanguage = (language) => {
+		i18n.changeLanguage(language);
+		// Stores language name in local storage to use in next session
+		localStorage.setItem("Language", language);
+	};
 
 	const actions = [
 		{ icon: <img src={EnglishIcon} alt="english-icon" width="40" />, name: "English", language: "en" },
