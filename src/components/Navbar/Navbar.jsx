fee7b1a0e3f245bb736e224836d507d54acd8775
Arian Hosseini <arianhosseini@outlook.com>
2022-08-27 21:14:12 +0430

    feat: complete Navbar Component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,55 @@
+import { Link, NavLink } from "react-router-dom";
+import { faApple } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { useTranslation } from "react-i18next";
+
 import styles from "./Navbar.module.css";
+import { faBagShopping } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
+	const { t, i18n } = useTranslation();
+
 	return (
 		<nav className={styles.navbar}>
 			<div className="container">
-				<h1>Hello, It's Navbar</h1>
+				<div className={styles.nav}>
+					<Link to="/" className={styles.logoLink}>
+						<FontAwesomeIcon icon={faApple} color="#d7d7d7" className={styles.logo} />
+					</Link>
+
+					<NavLink className={styles.navItem} to="/store">
+						{t("Store")}
+					</NavLink>
+					<NavLink className={styles.navItem} to="/mac">
+						{t("Mac")}
+					</NavLink>
+					<NavLink className={styles.navItem} to="/ipad">
+						{t("iPad")}
+					</NavLink>
+					<NavLink className={styles.navItem} to="/iphone">
+						{t("iPhone")}
+					</NavLink>
+					<NavLink className={styles.navItem} to="/watch">
+						{t("Watch")}
+					</NavLink>
+					<NavLink className={styles.navItem} to="/airpods">
+						{t("AirPods")}
+					</NavLink>
+					<NavLink className={styles.navItem} to="/tv">
+						{t("TV & Home")}
+					</NavLink>
+					<NavLink className={styles.navItem} to="/accessories">
+						{t("Accessories")}
+					</NavLink>
+					<NavLink className={styles.navItem} to="/support">
+						{t("Support")}
+					</NavLink>
+
+					<button className={styles.bagButton}>
+						<ion-icon name="bag-outline"></ion-icon>
+					</button>
+				</div>
 			</div>
 		</nav>
 	);
