eed4f229104e2f55f8b704947404da0857567d53
Arian Hosseini <arianhosseini@outlook.com>
2022-08-27 20:00:26 +0430

    refactor: fixed container max-width

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,13 @@
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<nav className={styles.navbar}>
+			<div className="container">
+				<h1>Hello, It's Navbar</h1>
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
