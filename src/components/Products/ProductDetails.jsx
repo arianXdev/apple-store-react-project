c6e90b5aef38765e668136e094592f233a2b0d87
Arian Hosseini <arianhosseini@outlook.com>
2022-08-29 02:48:13 +0430

    feat: improved Product Details Page

--- src/components/Products/ProductDetails.jsx
+++ src/components/Products/ProductDetails.jsx
@@ -1,13 +1,17 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useTransition } from "react";
 import { doc, getDoc, onSnapshot, collection } from "firebase/firestore";
 import db from "../../services/firebase";
 
 import { useParams } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+
 import styles from "./ProductDetails.module.css";
+import Spinner from "../Spinner/Spinner";
 
 const ProductDetails = () => {
 	const params = useParams();
 	const [productInfo, setProductInfo] = useState([]);
+	const [t, i18n] = useTranslation();
 
 	const docRef = doc(db, params.macId, params.macProductId);
 
@@ -20,8 +24,22 @@ const ProductDetails = () => {
 	);
 
 	return (
-		<div>
-			<h1>{productInfo.PersianName}</h1>
+		<div className={styles.ProductPage}>
+			<div className="container">
+				<section className={styles.ProductDetails}>
+					<div className={styles["ProductDetails__left"]}>{!productInfo.image ? <Spinner /> : <img src={productInfo.image} alt={productInfo.id || "product"} />}</div>
+					<div className={styles["ProductDetails__right"]} dir={t("dir")}>
+						<div className={styles["ProductDetails__info"]}>
+							<h2 className={styles["ProductDetails__title"]}>{t("dir") == "ltr" ? productInfo.EnglishName : productInfo.PersianName}</h2>
+							<h4 className={styles["ProductDetails__subtitle"]}>{productInfo.EnglishName}</h4>
+							<p className={styles["ProductDetails__price"]}>
+								{t("dir") == "ltr" ? `$${productInfo.dollarPrice}` : productInfo.rialPrice + " تومان" || "000,000,000"}
+							</p>
+						</div>
+						<button className={styles.AddToCardBtn}>{t("AddToCart")}</button>
+					</div>
+				</section>
+			</div>
 		</div>
 	);
 };
