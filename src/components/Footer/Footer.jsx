c1393d747da6a1cd51fe4ed4741ef65da8e0c8d3
Arian Hosseini <arianhosseini@outlook.com>
2022-08-29 18:24:06 +0430

    feat: added Footer Component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import styles from "./Footer.module.css";
+
+class Footer extends Component {
+	state = {};
+
+	render() {
+		return <footer className={styles.footer}>Hello Everybody! I'm Gregory from DApp University!</footer>;
+	}
+}
+
+export default Footer;
